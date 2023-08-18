a49c2e292b11038022d8601c364627fef99e40c8
phuoctm1910 <nothinginthis465@gmail.com>
2023-08-18 21:35:04 +0700

    Update login_phuoctmpc07090.php

--- login_phuoctmpc07090.php
+++ login_phuoctmpc07090.php
@@ -1,4 +1,4 @@
-<?php include'./header.php'?>
+<?php include './header.php' ?>
 <div class="vh-100 w-75 d-flex justify-content-center align-items-center m-auto">
     <div class="container">
         <div class="row d-flex justify-content-center">
@@ -6,16 +6,22 @@
                 <div class="border border-3 border-warning"></div>
                 <div class=" bg-white shadow-lg">
                     <div class="-body p-5">
-                        <form class="mb-3 mt-md-4">
+                        <form class="mb-3 mt-md-4 needs-validation" novalidate>
                             <h2 class="fw-bold mb-2 text-uppercase" style="color: #FFAC88;">FPhoto</h2>
                             <p class=" mb-5">Hãy nhập tài khoản và mật khẩu của bạn!</p>
                             <div class="mb-3">
                                 <label for="username" class="form-label ">Tên tài khoản:</label>
-                                <input type="text" class="form-control" id="text" placeholder="abc123">
+                                <input type="text" class="form-control" id="text" placeholder="abc123" require>
+                            </div>
+                            <div class="invalid-feedback">
+                                Không được bỏ trống Username
                             </div>
                             <div class="mb-3">
                                 <label for="password" class="form-label ">Mật Khẩu:</label>
-                                <input type="password" class="form-control" id="password" placeholder="*******">
+                                <input type="password" class="form-control" id="password" placeholder="*******" require>
+                            </div>
+                            <div class="invalid-feedback">
+                                Không được bỏ trống mật khẩu
                             </div>
                             <p class="small"><a class="text-primary" href="forget-password.html">Quên Mật Khẩu?</a></p>
                             <div class="d-grid">
@@ -31,3 +37,24 @@
         </div>
     </div>
 </div>
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
+</script>
