9245cb9f6fe2fde4dbf6507136ba7eff8cdbd57d
phuoctm1910 <nothinginthis465@gmail.com>
2023-08-23 22:32:43 +0700

    update deleteData_phuoctmpc07090

--- client/components/masonry_vuntpc07201.php
+++ client/components/masonry_vuntpc07201.php
@@ -3,7 +3,7 @@
 
         <h3>Ảnh đã chọn</h3>
         <form action="./action/send-image_vuntpc07201.php" method="post">
-            <input type="hidden" id="imageSelected" name="imageSelected" >
+            <input type="hidden" id="imageSelected" name="imageSelected">
             <button class="btn btn-primary" name="send">Gửi</button>
         </form>
     </div>
@@ -13,20 +13,34 @@
 </div>
 <script>
     let selectedImages = JSON.parse(localStorage.getItem("selectedImages")) || [];
-    let input =document.getElementById('imageSelected')
+    let input = document.getElementById('imageSelected')
     let masonry = document.getElementById('masonry')
     let html = '';
+    
     getData()
+
     function getData() {
         input.value = selectedImages
         selectedImages.forEach(element => {
             html += `
-            <div class="masonry-item mb-3">
+            <div class="masonry-item mb-3 position-relative">
                 <img src="../uploads/${element}" class="carousel-image w-100 rounded-3">
+                <button class="position-absolute top-0 end-0 btn btn-danger" onclick="deleteData(event)">X</button>
             </div>
             `
         });
         masonry.innerHTML = html
     }
 
+    function deleteData(event) {
+        let clickedButton = event.target;
+        let imgSrc = clickedButton.previousElementSibling.getAttribute("src").split("/")[2];
+        let imgIndex = selectedImages.indexOf(imgSrc);
+
+        if (imgIndex !== -1) {
+            selectedImages.splice(imgIndex, 1);
+            localStorage.setItem("selectedImages", JSON.stringify(selectedImages));
+            clickedButton.parentElement.remove();
+        }
+    }
 </script>
